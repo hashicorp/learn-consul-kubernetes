d92939abaab047d3102a97136541c2df9c8f0a74
DerekStrickland <1111455+DerekStrickland@users.noreply.github.com>
2021-03-22 13:12:20 -0400

    metrics updates

--- jaeger/init.sh
+++ jaeger/init.sh
@@ -1,14 +1,28 @@
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm repo udpate
 
-kind create cluster --name hashicups-jaeger
+
+kind delete cluster --name hashicups-jaeger
+kind create cluster --name hashicups-jaeger --config ./helm/kind.yaml
 
 helm install -f ./helm/consul.yaml consul hashicorp/consul --version "0.30.0" --wait
 
 kubectl apply -f proxy-defaults.yaml
 
-helm install jaeger jaegertracing/jaeger-operator --wait
+helm install jaeger jaegertracing/jaeger-operator --version "2.19.1" --wait
+helm upgrade -i jaeger-operator jaegertracing/jaeger-operator --wait
+
+#kubectl create namespace observability
+#kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing.io_jaegers_crd.yaml
+#kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
+#kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
+#kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml
+#kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/operator.yaml
+#
+#kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/cluster_role.yaml
+#kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/cluster_role_binding.yaml
 
 kubectl apply -f ./helm/jaeger.yaml --wait
 
