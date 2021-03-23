15159a2727bafa68ca9a8822c169243b2a31ef83
Derek Strickland <1111455+DerekStrickland@users.noreply.github.com>
2021-03-23 12:37:05 -0400

    Consul helm 0 30 0 (#16)
    * add service-service-mesh observability support
    
    Co-authored-by: Luke Kysow <1034429+lkysow@users.noreply.github.com>

--- jaeger/init.sh
+++ jaeger/init.sh
@@ -1,15 +1,18 @@
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm repo udpate
 
-kind create cluster --name hashicups-jaeger
+
+kind delete cluster --name hashicups-jaeger
+kind create cluster --name hashicups-jaeger --config ./helm/kind.yaml
 
 helm install -f ./helm/consul.yaml consul hashicorp/consul --version "0.30.0" --wait
-
 kubectl apply -f proxy-defaults.yaml
 
-helm install jaeger jaegertracing/jaeger-operator --wait
+helm install jaeger jaegertracing/jaeger-operator --version "2.19.1" --wait
+helm upgrade -i jaeger-operator jaegertracing/jaeger-operator --wait
+
 
 kubectl apply -f ./helm/jaeger.yaml --wait
-
 kubectl apply -f ./ --wait
