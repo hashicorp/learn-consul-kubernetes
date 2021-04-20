30a26436f8c47ac99695c70c158245ca6be63ec9
Derek Strickland <1111455+DerekStrickland@users.noreply.github.com>
2021-04-20 10:23:26 -0400

    zero-trust-network supporting code - part of vault-ca (#18)

---
+++ vault-certificate-authority/init.sh
@@ -0,0 +1,32 @@
+VAULT_ADDR=$1
+VAULT_TOKEN=$2
+
+helm repo update
+
+kubectl config use-context dc1
+
+cat <<EOF > ./ca-config.json
+{
+  "connect": [
+    {
+      "ca_config": [
+        {
+          "address": "$VAULT_ADDR",
+          "intermediate_pki_path": "dc1/connect-intermediate",
+          "root_pki_path": "connect-root",
+          "token": "$VAULT_TOKEN"
+        }
+      ],
+      "ca_provider": "vault"
+    }
+  ]
+}
+EOF
+
+kubectl create secret generic vault-config --from-file=config=./ca-config.json
+
+kubectl create secret generic consul-gossip-encryption-key --from-literal=key=$(consul keygen)
+
+helm install consul hashicorp/consul -f ./config.yaml --version "0.31.1" --wait
+
+
