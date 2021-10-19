774144bc4313fd23498d240f7710aed790fe9eda
Karl Cardenas <kcardenas@hashicorp.com>
2021-10-19 15:30:15 -0700

    fix: updating the eks-clients folder

--- consul-client-eks/variables.tf
+++ consul-client-eks/variables.tf
@@ -1,18 +1,37 @@
-variable "vpc_id" {
-  description = "VPC ID of VPC you have peered with your HCP HVN"
-}
-
 variable "region" {
-  default = "us-west-2"
+  default = "us-east-1"
   description = "The region"
 }
 
 variable "datacenter_name" {
-  default = "hcp-dc1"
+  default = "dc1"
   description = "Name of the Consul datacenter to create"
 }
 
 variable "output_dir" {
   default = "."
   description = "The directory to store output artifacts like kubeconfig files"
+}
+
+variable "kubernetes_version" {
+  type = string
+  description = "The version of Kubernetes to use"
+  default = "1.21"
+}
+
+variable "ec2_instance_type" {
+  type = string
+  description = "The underlying EC2 instance type to use"
+  default = "t2.medium"
+}
+
+variable "default_tags" {
+  description = "Default Tags for AWS"
+  type        = map(string)
+  default = {
+    Environment       = "dev"
+    Team              = "Education-Consul"
+    terraform_managed = true
+    deletePrevention  = false
+  }
 }
