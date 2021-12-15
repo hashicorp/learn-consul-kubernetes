b2e9121309642cf3f1876cbe8b61ba9d6807da4e
Christian Weber <christian.weber@hashicorp.com>
2021-12-15 12:05:11 -0600

    update script

--- consul-enterpise-admin-partitions-eks/consul_enterprise/install_consul.sh
+++ consul-enterpise-admin-partitions-eks/consul_enterprise/install_consul.sh
@@ -11,12 +11,15 @@ if [ "${CONSUL_DEPLOY_TYPE}" = "server" ]; then
 else
   kubectl config use-context "${PRIMARY}"
 
-  kubectl get secret consul-bootstrap-acl-token --context "${PRIMARY}" -o yaml | kubectl apply --context "${SECONDARY}" -f -
+  #kubectl get secret consul-bootstrap-acl-token --context "${PRIMARY}" -o yaml | kubectl apply --context "${SECONDARY}" -f -
   kubectl get secret consul-ca-key --context "${PRIMARY}" -o yaml | kubectl apply --context "${SECONDARY}" -f -
   kubectl get secret consul-ca-cert --context "${PRIMARY}" -o yaml | kubectl apply --context "${SECONDARY}" -f -
   PartitionIp=$(kubectl get svc | grep -i "LoadBalancer" | awk \{'print $4'\})
+  # TERM=dumb removes ANSI color coding from this command. See: https://github.com/kubernetes/kubernetes/issues/5698#issuecomment-566125254
+  #ControlPlaneHost=$(TERM=dumb kubectl cluster-info | grep -i "kubernetes control plane" | awk \{'print $NF'\})
+  #yq e ".externalServers.k8sAuthMethodHost = \"${ControlPlaneHost}\"" -i ./consul-values-"${CONSUL_DEPLOY_TYPE}".yaml
   yq e ".client.join = [\"${PartitionIp}\"]" -i ./consul-values-"${CONSUL_DEPLOY_TYPE}".yaml
   yq e ".externalServers.hosts = [\"${PartitionIp}\"]" -i ./consul-values-"${CONSUL_DEPLOY_TYPE}".yaml
   kubectl config use-context "${SECONDARY}"
-  helm install --wait hashicorp-"${CONSUL_DEPLOY_TYPE}" hashicorp/consul -f ./consul_enterprise/consul-values-"${CONSUL_DEPLOY_TYPE}".yaml
+  helm install --wait hashicorp-"${CONSUL_DEPLOY_TYPE}" hashicorp/consul -f ./consul-values-"${CONSUL_DEPLOY_TYPE}".yaml
 fi
