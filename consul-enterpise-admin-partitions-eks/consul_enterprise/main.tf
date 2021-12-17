45b0ddd64aac65cb59fdab733595b57bc6b198e7
Christian Weber <christian.weber@hashicorp.com>
2021-12-17 14:20:49 -0600

    remove auto installation of certs, comment out automatical consul enterprise installation in the null resource. Simplify naming for license

--- consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
+++ consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
@@ -3,10 +3,10 @@ variable "eks_cluster_name" {}
 variable "deploy_type" {}
 variable "eks_cluster_primary" {}
 
-variable "license_content_path" {}
+variable "license_name" {}
 
 locals {
-  license_content = file("${path.root}/${path.module}/${var.license_content_path}")
+  license_content = file("${path.root}/${path.module}/${var.license_name}")
 }
 
 resource "null_resource" "install_consul_enterprise" {
@@ -22,20 +22,20 @@ resource "null_resource" "install_consul_enterprise" {
     working_dir = path.module
     command     = "kubectl config use-context ${var.eks_cluster_name} && kubectl create secret generic consul-ent-license --from-literal=\"key=${local.license_content}\""
   }
-  provisioner "local-exec" {
-    working_dir = path.module
-    command     = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; export CURRENT_KUBE_CONTEXT=${var.eks_cluster_name}; export CONSUL_DEPLOY_TYPE=${var.deploy_type}; bash ./install_consul.sh"
-  }
-
-  provisioner "local-exec" {
-    command = "bash ./consul_enterprise/install_certs.sh"
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    # This solves an issue where k8s deploys an AWS resource (ELB) that terraform can't control and will error out the destroy command
-    command = "kubectl get services --all-namespaces | grep -vi kube | grep -i LoadBalancer | awk {'print $1'} | sed -n '1d;p' | xargs kubectl delete services"
-  }
+#  provisioner "local-exec" {
+#    working_dir = path.module
+#    command     = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; export CURRENT_KUBE_CONTEXT=${var.eks_cluster_name}; export CONSUL_DEPLOY_TYPE=${var.deploy_type}; bash ./install_consul.sh"
+#  }
+#
+#  provisioner "local-exec" {
+#    command = "bash ./consul_enterprise/install_certs.sh"
+#  }
+#
+#  provisioner "local-exec" {
+#    when = destroy
+#    # This solves an issue where k8s deploys an AWS resource (ELB) that terraform can't control and will error out the destroy command
+#    command = "kubectl get services --all-namespaces | grep -vi kube | grep -i LoadBalancer | awk {'print $1'} | sed -n '1d;p' | xargs kubectl delete services"
+#  }
   #  provisioner "local-exec" {
   #    when = destroy
   #    # This solves an issue where k8s deploys an AWS resource (ELB) that terraform can't control and will error out the destroy command
