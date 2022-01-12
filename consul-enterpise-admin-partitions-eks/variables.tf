c10fbdbad68e59de80ad880a09e7debd1b6d9aa3
Christian Weber <christian.weber@hashicorp.com>
2022-01-12 11:58:23 -0600

    remove unused vars, change intent of consul ent license file

--- consul-enterpise-admin-partitions-eks/variables.tf
+++ consul-enterpise-admin-partitions-eks/variables.tf
@@ -1,29 +1,21 @@
-variable "author" {
-  type = string
-}
-variable "team" {
-  type = string
-}
-variable "eks_cluster_name" {
-  type = string
-}
 variable "eks_cluster_name_1" {
   type = string
 }
 variable "eks_cluster_name_2" {
   type = string
 }
-variable "license_file_path" {
+variable "license_name" {
   type        = string
-  description = "The path to the license file"
-  default     = "./consul.hclic"
+  description = "The name of the Consul Enterprise license file"
+  default     = "consul.hclic"
 }
 
 variable "default_tags" {
   type = map(string)
   default = {
-    Team       = "education" #var.deploymentInfo.team
-    deployedBy = "webdog"    #var.deploymentInfo.author
+    Team       = "education"
+    deployedBy = "webdog"
+    TTL        = "unlimited"
   }
 }
 
