45efd2dee8483cc36c0dc6a69ffdb54a6e33843b
Christian Weber <christian.weber@hashicorp.com>
2022-01-12 17:56:05 -0600

    normalized variable names

--- consul-enterpise-admin-partitions-eks/main.tf
+++ consul-enterpise-admin-partitions-eks/main.tf
@@ -4,89 +4,97 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.66.0"
     }
-    #    TODO: Add this into for explicit required providers
-    #    null = {
-    #      source = "hashicorp/null"
-    #      version = "~> 4.1.0"
-    #    }
   }
 }
-variable "eks_cluster_primary_ips_starfleet" {}
-variable "eks_cluster_secondary_ips_terek_nor" {}
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
   default_tags {
     tags = var.default_tags
   }
-  #version = "3.66.0"
 }
 
-module "starfleet_k8s" {
+# module "starfleet_k8s"
+module "primary_kubernetes" {
   source = "./k8s"
   # starfleet
-  eks_cluster_name             = var.eks_cluster_name_1
-  eks_vpc_cidr_block           = var.eks_cluster_primary_ips_starfleet
-  eks_vpc_cidr_block_starfleet = var.eks_cluster_primary_ips_starfleet
-  eks_vpc_cidr_block_terek_nor = var.eks_cluster_secondary_ips_terek_nor
-  eks_primary_cluster          = var.eks_cluster_name_1
-}
-module "terek_nor_k8s" {
-  source = "./k8s"
-  # terek_nor
-  eks_cluster_name             = var.eks_cluster_name_2
-  eks_vpc_cidr_block           = var.eks_cluster_secondary_ips_terek_nor
-  eks_vpc_cidr_block_starfleet = var.eks_cluster_primary_ips_starfleet
-  eks_vpc_cidr_block_terek_nor = var.eks_cluster_secondary_ips_terek_nor
-  eks_primary_cluster          = var.eks_cluster_name_1
-  depends_on                   = [module.starfleet_k8s]
+  eks_cluster_name   = var.eks_cluster_name_primary
+  eks_vpc_cidr_block = var.eks_cluster_primary_ips_consul_server
+  #eks_vpc_cidr_block_starfleet = var.eks_cluster_primary_ips_starfleet - DELETE
+  eks_vpc_cidr_block_primary = var.eks_cluster_primary_ips_consul_server
+  #eks_vpc_cidr_block_terek_nor = var.eks_cluster_secondary_ips_terek_nor - DELETE
+  eks_vpc_cidr_block_secondary = var.eks_cluster_secondary_ips_consul_client
+  eks_primary_cluster          = var.eks_cluster_name_primary
+  availability_zones           = var.availability_zones
+
 }
 
-module "create_peering_terek_nor_to_starfleet" {
-  source           = "./peering"
-  vpc_id_accepter  = module.starfleet_k8s.vpc_id
-  vpc_id_requester = module.terek_nor_k8s.vpc_id
+# module "terek_nor_k8s"
+module "secondary_kubernetes" {
+  source = "./k8s"
+  # terek_nor
+  eks_cluster_name   = var.eks_cluster_name_secondary
+  eks_vpc_cidr_block = var.eks_cluster_secondary_ips_consul_client
+  #eks_vpc_cidr_block_starfleet = var.eks_cluster_primary_ips_starfleet - DELETE
+  eks_vpc_cidr_block_primary = var.eks_cluster_primary_ips_consul_server
+  #eks_vpc_cidr_block_terek_nor = var.eks_cluster_secondary_ips_terek_nor - DELETE
+  eks_vpc_cidr_block_secondary = var.eks_cluster_secondary_ips_consul_client
+  eks_primary_cluster          = var.eks_cluster_name_secondary
+  depends_on                   = [module.primary_kubernetes]
+  availability_zones           = var.availability_zones
 }
-module "post_route_terek_nor_to_starfleet" {
-  source = "./post_routing"
-  # Creates the route for the peering connection from terek_nor's perspective
-  vpc_peering_connection_id = module.create_peering_terek_nor_to_starfleet.vpc_peering_connection_id
-  routing_table_id          = module.terek_nor_k8s.route_table_id
-  vpc_id                    = module.terek_nor_k8s.vpc_id
-  cidr_block_transit        = var.eks_cluster_primary_ips_starfleet.vpc
-  main_route_table          = module.terek_nor_k8s.main_route_table_id
-  private_route_table       = module.terek_nor_k8s.route_table_id
+
+# module create_peering_terek_nor_to_starfleet
+module "create_peering_secondary_to_primary" {
+  source           = "./peering"
+  vpc_id_accepter  = module.primary_kubernetes.vpc_id
+  vpc_id_requester = module.secondary_kubernetes.vpc_id
 }
 
 # Sets up routing for VPC Peering. Runs after VPCs have been created.
-module "post_route_starfleet_to_terek_nor" {
+# Creates the route for the peering connection from secondary's perspective
+# module module "post_route_terek_nor_to_starfleet"
+module "post_vpc_creation_routing_rules_secondary_to_primary" {
   source                    = "./post_routing"
-  vpc_peering_connection_id = module.create_peering_terek_nor_to_starfleet.vpc_peering_connection_id
-  routing_table_id          = module.starfleet_k8s.route_table_id
-  vpc_id                    = module.starfleet_k8s.vpc_id
-  cidr_block_transit        = var.eks_cluster_secondary_ips_terek_nor.vpc
-  main_route_table          = module.starfleet_k8s.main_route_table_id
-  private_route_table       = module.starfleet_k8s.route_table_id
+  vpc_peering_connection_id = module.create_peering_secondary_to_primary.vpc_peering_connection_id
+  routing_table_id          = module.secondary_kubernetes.route_table_id
+  vpc_id                    = module.secondary_kubernetes.vpc_id
+  cidr_block_transit        = var.eks_cluster_primary_ips_consul_server.vpc
+  main_route_table          = module.secondary_kubernetes.main_route_table_id
+  private_route_table       = module.secondary_kubernetes.route_table_id
 }
 
-module "install_consul_enterprise" {
+# Sets up routing for VPC Peering. Runs after VPCs have been created.
+# Creates the route for the peering connection from secondary's perspective
+#module "post_route_starfleet_to_terek_nor"
+module "post_vpc_creation_routing_rules_primary_to_secondary" {
+  source                    = "./post_routing"
+  vpc_peering_connection_id = module.create_peering_secondary_to_primary.vpc_peering_connection_id
+  routing_table_id          = module.primary_kubernetes.route_table_id
+  vpc_id                    = module.primary_kubernetes.vpc_id
+  cidr_block_transit        = var.eks_cluster_secondary_ips_consul_client.vpc
+  main_route_table          = module.primary_kubernetes.main_route_table_id
+  private_route_table       = module.primary_kubernetes.route_table_id
+}
+
+module "install_consul_enterprise_server" {
   source              = "./consul_enterprise"
-  eks_cluster_name    = var.eks_cluster_name_1
-  eks_cluster_primary = var.eks_cluster_name_1
-  deploy_type         = "server"
+  eks_cluster_name    = var.eks_cluster_name_primary
+  eks_cluster_primary = var.eks_cluster_name_primary
+  deploy_type         = var.deploy_type.server
   license_name        = var.license_name
-  cluster_subnet_id   = module.starfleet_k8s.public-subnet-id
-  vpc_id              = module.starfleet_k8s.vpc_id
-  depends_on          = [module.starfleet_k8s]
+  cluster_subnet_id   = module.primary_kubernetes.public-subnet-id
+  vpc_id              = module.primary_kubernetes.vpc_id
+  depends_on          = [module.primary_kubernetes]
 
 }
-module "install_consul_enterprise_secondary" {
+module "install_consul_enterprise_client" {
   source              = "./consul_enterprise"
-  eks_cluster_name    = var.eks_cluster_name_2
-  eks_cluster_primary = var.eks_cluster_name_1
-  deploy_type         = "client"
+  eks_cluster_name    = var.eks_cluster_name_secondary
+  eks_cluster_primary = var.eks_cluster_name_primary
+  deploy_type         = var.deploy_type.client
   license_name        = var.license_name
-  cluster_subnet_id   = module.terek_nor_k8s.public-subnet-id
-  vpc_id              = module.terek_nor_k8s.vpc_id
-  depends_on          = [module.terek_nor_k8s, module.install_consul_enterprise]
+  cluster_subnet_id   = module.secondary_kubernetes.public-subnet-id
+  vpc_id              = module.secondary_kubernetes.vpc_id
+  depends_on          = [module.secondary_kubernetes, module.install_consul_enterprise_server]
 }
--- consul-enterpise-admin-partitions-eks/variables.tf
+++ consul-enterpise-admin-partitions-eks/variables.tf
@@ -1,21 +1,65 @@
-variable "eks_cluster_name_1" {
-  type = string
+variable "eks_cluster_name_primary" {
+  description = "Name of the primary EKS Cluster that runs the Consul Enterprise server cluster"
+  type        = string
+  default     = "Server"
 }
-variable "eks_cluster_name_2" {
-  type = string
+variable "eks_cluster_name_secondary" {
+  type        = string
+  description = "Name of the secondary EKS Cluster that runs the Consul Enterprise client cluster"
+  default     = "Client"
 }
 variable "license_name" {
   type        = string
-  description = "The name of the Consul Enterprise license file"
+  description = "The name of the Consul Enterprise license file. This file is placed in the `./consul_enterprise/` directory"
   default     = "consul.hclic"
 }
 
+# Default tags to pass to AWS. You can set them here, or in a tfvars file.
 variable "default_tags" {
   type = map(string)
+}
+
+variable "eks_cluster_primary_ips_consul_server" {
+  #variable "eks_cluster_primary_ips_starfleet" {
+  description = "The CIDR groups for the Primary Cluster's IP addresses"
   default = {
-    Team       = "education"
-    deployedBy = "webdog"
-    TTL        = "unlimited"
+    vpc      = "10.100.0.0/16"
+    private  = "10.100.1.0/24"
+    public   = "10.100.2.0/24"
+    internet = "0.0.0.0/0"
   }
 }
 
+variable "eks_cluster_secondary_ips_consul_client" {
+  #variable "eks_cluster_secondary_ips_terek_nor" {
+  description = "The CIDR groups for the Secondary Cluster's IP addresses"
+  default = {
+    vpc      = "172.30.0.0/16"
+    private  = "172.30.1.0/24"
+    public   = "172.30.2.0/24"
+    internet = "0.0.0.0/0"
+  }
+}
+
+# The name of the Consul Enterprise installation types. If this comment is here
+# please don't change these values. The configs are currently reliant on these namse
+# which will be addressed at a later time.
+variable "deploy_type" {
+  default = {
+    server = "server"
+    client = "client"
+  }
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "availability_zones" {
+  description = "The AZs in which the Clusters will deploy"
+  default = {
+    # us-east-1a
+    zone_one = "us-east-1a"
+    zone_two = "us-east-1f"
+  }
+}
