1f1c63557e88d5651abd9f62c0787079f7d28521
Christian Weber <christian.weber@hashicorp.com>
2022-01-12 17:55:48 -0600

    normalized variable names

--- consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
+++ consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
@@ -1,16 +1,8 @@
-
-variable "eks_cluster_name" {}
-variable "deploy_type" {}
-variable "eks_cluster_primary" {}
-variable "cluster_subnet_id" {}
-variable "vpc_id" {}
-
-variable "license_name" {}
-
 locals {
   license_content     = file("${path.root}/${path.module}/${var.license_name}")
 }
 
+# This module only runs when `terraform destroy` is invoked.
 module "cleanup" {
   source = "./cleanup"
   vpc_id = var.vpc_id
@@ -21,6 +13,7 @@ module "cleanup" {
 
 resource "null_resource" "install_consul_enterprise" {
   provisioner "local-exec" {
+    # The aws command line will update your kubeconfig with your cluster info, so you can use `kubectl` locally.
     command = "aws eks update-kubeconfig --region ${data.aws_region.current.name} --name ${var.eks_cluster_name} --alias ${var.eks_cluster_name}"
   }
   # Install the k8s dashboard and EKS Service Accounts
@@ -32,26 +25,17 @@ resource "null_resource" "install_consul_enterprise" {
     working_dir = path.module
     command     = "kubectl config use-context ${var.eks_cluster_name} && kubectl create secret generic consul-ent-license --from-literal=\"key=${local.license_content}\""
   }
+  # The Consul Enterprise Installation Script
   provisioner "local-exec" {
     working_dir = path.module
-    command     = "pwd && export CONSUL_PRIMARY=${var.eks_cluster_primary}; export CURRENT_KUBE_CONTEXT=${var.eks_cluster_name}; export CONSUL_DEPLOY_TYPE=${var.deploy_type}; bash install_consul.sh"
+    command     = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; export CURRENT_KUBE_CONTEXT=${var.eks_cluster_name}; export CONSUL_DEPLOY_TYPE=${var.deploy_type}; bash install_consul.sh"
   }
 
+  # Generates certs locally for your Consul Enterprise installation, if you want to run things like consul proxy.
   provisioner "local-exec" {
     working_dir = path.module
     command = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; bash install_certs.sh"
   }
 
-  #
-  #  provisioner "local-exec" {
-  #    when = destroy
-  #    # This solves an issue where k8s deploys an AWS resource (ELB) that terraform can't control and will error out the destroy command
-  #    command = "kubectl get services --all-namespaces | grep -vi kube | grep -i LoadBalancer | awk {'print $1'} | sed -n '1d;p' | xargs kubectl delete services"
-  #  }
-  #  provisioner "local-exec" {
-  #    when = destroy
-  #    # This solves an issue where k8s deploys an AWS resource (ELB) that terraform can't control and will error out the destroy command
-  #    command = "kubectl get deployments --all-namespaces | grep -v consul  | grep -v kube-system| awk {'print $1'} | sed -n '1d;p' | xargs kubectl delete deployments"
-  #  }
 }
 
