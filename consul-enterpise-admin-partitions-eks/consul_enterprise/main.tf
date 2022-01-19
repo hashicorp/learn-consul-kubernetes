6199eafba32e0aaa821c376094d370b3dec7e67a
Christian Weber <christian.weber@hashicorp.com>
2022-01-19 10:17:46 -0600

    fix comment and remove auto cert install. The script can stay as a convenience, but won't run automatically

--- consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
+++ consul-enterpise-admin-partitions-eks/consul_enterprise/main.tf
@@ -25,16 +25,10 @@ resource "null_resource" "install_consul_enterprise" {
     working_dir = path.module
     command     = "kubectl config use-context ${var.eks_cluster_name} && kubectl create secret generic consul-ent-license --from-literal=\"key=${local.license_content}\""
   }
-  # The Consul Enterprise Installation Script
+  #The Consul Enterprise Installation Script
   provisioner "local-exec" {
     working_dir = path.module
     command     = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; export CURRENT_KUBE_CONTEXT=${var.eks_cluster_name}; export CONSUL_DEPLOY_TYPE=${var.deploy_type}; bash install_consul.sh"
-  }
-
-  # Generates certs locally for your Consul Enterprise installation, if you want to run things like consul proxy.
-  provisioner "local-exec" {
-    working_dir = path.module
-    command = "export CONSUL_PRIMARY=${var.eks_cluster_primary}; bash install_certs.sh"
   }
 
 }
