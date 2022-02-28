9e9acb11b879c19362ad24950fbff1233577aa8b
Christian Weber <christian.weber@hashicorp.com>
2022-02-28 14:09:51 -0600

    removing the granular security groups, as it was causing too much issue with traffic between all the interlinked services

--- hcp-consul-hcp-vault-ca/modules/aws/main.tf
+++ hcp-consul-hcp-vault-ca/modules/aws/main.tf
@@ -1,71 +1,17 @@
 resource "aws_security_group" "open" {
   vpc_id = var.aws_vpc_id
   ingress {
-    description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["216.80.1.157/32"]
-  }
-  ingress {
-    from_port = 8301
-    protocol  = "tcp"
-    to_port   = 8301
-    cidr_blocks = [var.hvn_cidr]
-  }
-  ingress {
-    from_port = 8301
-    protocol  = "udp"
-    to_port   = 8301
-    cidr_blocks = [var.hvn_cidr]
-  }
-  ingress {
-    from_port = 8301
-    protocol  = "tcp"
-    to_port   = 8301
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
     self = true
   }
+
   ingress {
-    from_port = 8301
-    protocol  = "udp"
-    to_port   = 8301
-    self = true
-  }
-  egress {
-    from_port = 8300
-    protocol  = "tcp"
-    to_port   = 8300
-    cidr_blocks = [var.hvn_cidr]
-  }
-  egress {
-    from_port = 8301
-    protocol  = "tcp"
-    to_port   = 8301
-    cidr_blocks = [var.hvn_cidr]
-  }
-  egress {
-    from_port = 8301
-    protocol  = "udp"
-    to_port   = 8301
-    cidr_blocks = [var.hvn_cidr]
-  }
-  egress {
-    from_port = 8301
-    protocol  = "tcp"
-    to_port   = 8301
-    self = true
-  }
-  egress {
-    from_port = 8301
-    protocol  = "udp"
-    to_port   = 8301
-    self = true
-  }
-  egress {
-    from_port = 443
-    protocol  = "tcp"
-    to_port   = 443
-    cidr_blocks = [var.hvn_cidr]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -74,7 +20,51 @@ resource "aws_security_group" "open" {
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol  = "-1"
+    self = true
+  }
+
+
 }
+
+resource "aws_security_group" "hashicups_kubernetes" {
+  vpc_id = var.aws_vpc_id
+
+  ingress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+   self = true
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    self = true
+  }
+
+}
+
+
 
 
 
--- hcp-consul-hcp-vault-ca/modules/aws/outputs.tf
+++ hcp-consul-hcp-vault-ca/modules/aws/outputs.tf
@@ -1,3 +1,7 @@
 output "aws_security_group_id" {
   value = aws_security_group.open.id
 }
+
+output "aws_hashicups_sg" {
+  value = aws_security_group.hashicups_kubernetes.id
+}
