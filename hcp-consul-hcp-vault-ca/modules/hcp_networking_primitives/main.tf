14478b69ae4f240531046034e6276c81af45acdf
Christian Weber <christian.weber@hashicorp.com>
2022-03-10 12:20:18 -0600

    cleanup, formatting, comments

--- hcp-consul-hcp-vault-ca/modules/hcp_networking_primitives/main.tf
+++ hcp-consul-hcp-vault-ca/modules/hcp_networking_primitives/main.tf
@@ -1,7 +1,8 @@
+# The HCP HVN Resource. Hosts the HCP Vault and Cluster instances
 resource "hcp_hvn" "server" {
   cloud_provider = var.cloud_provider
   hvn_id         = var.hvn_name
   region         = var.hcp_region
-  cidr_block =   var.cidr_block
+  cidr_block     = var.cidr_block
 }
 
--- hcp-consul-hcp-vault-ca/modules/hcp_networking_primitives/variables.tf
+++ hcp-consul-hcp-vault-ca/modules/hcp_networking_primitives/variables.tf
@@ -1,19 +1,20 @@
 variable "cloud_provider" {
-  type = string
+  type        = string
   description = "Cloud Provider"
-  default = "aws"
+  default     = "aws"
 }
 
 variable "hvn_name" {
-  type = string
+  type        = string
   description = "Name given to the HVN"
 }
+
 variable "hcp_region" {
-  type = string
+  type        = string
   description = "Region for HCP"
 }
 
 variable "cidr_block" {
-  type = string
+  type        = string
   description = "CIDR block for HVN"
 }
