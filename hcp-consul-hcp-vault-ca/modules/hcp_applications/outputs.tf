f576544afaa1f09360d7bcf5192d35241dc375ac
Christian Weber <christian.weber@hashicorp.com>
2022-03-10 12:20:50 -0600

    Cleanup, unused variables, formatting

--- hcp-consul-hcp-vault-ca/modules/hcp_applications/outputs.tf
+++ hcp-consul-hcp-vault-ca/modules/hcp_applications/outputs.tf
@@ -1,31 +1,39 @@
+# Endpoint to access HCP Consul
 output "consul_cluster_host" {
   value = hcp_consul_cluster.server.consul_private_endpoint_url
 }
 
+# The Consul config file to setup the working environment
 output "consul_config_file" {
   value = hcp_consul_cluster.server.consul_config_file
 }
 
+# The Consul root CA file
 output "consul_ca_file" {
   value = hcp_consul_cluster.server.consul_ca_file
 }
 
+# The ID for the for Consul root token
 output "consul_root_token_accessor_id" {
   value = hcp_consul_cluster_root_token.user.accessor_id
 }
 
+# The Consul root token
 output "consul_root_token" {
   value = hcp_consul_cluster_root_token.user.kubernetes_secret
 }
 
+# The secret ID for the Consul root token
 output "consul_root_token_secret_id" {
   value = hcp_consul_cluster_root_token.user.secret_id
 }
 
+# The Private Endpoint for HCP Vault
 output "vault_cluster_host" {
   value = hcp_vault_cluster.consul_backend.vault_private_endpoint_url
 }
 
+# The Vault admin token
 output "vault_admin_token" {
   value = hcp_vault_cluster_admin_token.user.token
 }
--- hcp-consul-hcp-vault-ca/modules/hcp_applications/variables.tf
+++ hcp-consul-hcp-vault-ca/modules/hcp_applications/variables.tf
@@ -1,38 +1,36 @@
 variable "consul_cluster_datacenter" {
-  description = "Name of the Consul DC"
-  type = string
+  description = "Name of the Consul datacenter"
+  type        = string
 }
 
 variable "vault_cluster_name" {
   description = "Vault cluster name"
-  type = string
+  type        = string
 }
 
 variable "hvn_id" {
   description = "Virtual Network in HCP"
-  type = string
+  type        = string
 }
 
 variable "consul_public_endpoint" {
   description = "Publicly accessible endpoint for Consul"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "vault_public_endpoint" {
   description = "Publicly accessible endpoint for Vault"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "hcp_consul_tier" {
   description = "Tier of HCP Consul"
-  default = "standard"
-  type = string
+  type        = string
 }
 
 variable "hcp_vault_tier" {
   description = "Tier for HCP Vault"
-  default = "starter_small"
-  type = string
+  type        = string
 }
