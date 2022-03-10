d5ad86a0cea5b9ca33191dd8cd7baf0d8f987d43
Christian Weber <christian.weber@hashicorp.com>
2022-03-10 12:21:38 -0600

    cleaning up comments, formatting

--- hcp-consul-hcp-vault-ca/modules/cleanup/main.tf
+++ hcp-consul-hcp-vault-ca/modules/cleanup/main.tf
@@ -1,7 +1,8 @@
+# See note in the triggers map in the null_resource block
 locals {
-  vpc_id              = var.vpc_id
-  cluster_name        = var.cluster_name
-  cleanup             = var.start_cleanup
+  vpc_id       = var.vpc_id
+  cluster_name = var.cluster_name
+  cleanup      = var.start_cleanup
 }
 
 
@@ -15,7 +16,9 @@ resource "null_resource" "cleanup" {
     cleanup              = local.cleanup
   }
 
-  # LoadBalancers and their pesky ENIs like to stick around for far too long. Clean these up before the rest of the infrastructure is torn down.
+  # LoadBalancers and associated ENIs can persist during a delete, erroring out. Removes the ENIs and LoadBalancers
+  # explicitly before the rest of the infrastructure is removed. Also cleans up kubeconfig setup and the
+  # dynamically generated tfvars file in ../../working-environment
   provisioner "local-exec" {
     when        = destroy
     working_dir = path.module
@@ -25,7 +28,7 @@ resource "null_resource" "cleanup" {
       CLUSTER_NAME       = self.triggers.cluster_name
       CLEANUP            = self.triggers.cleanup
     }
-    command     = "bash cleanup.sh"
+    command = "bash cleanup.sh"
 
   }
 }
--- hcp-consul-hcp-vault-ca/modules/cleanup/variables.tf
+++ hcp-consul-hcp-vault-ca/modules/cleanup/variables.tf
@@ -1,6 +1,19 @@
-variable "vpc_id" {}
-variable "region" {}
-variable "cluster_name" {}
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to cleanup"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to cleanup"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS Cluster to cleanup"
+}
+
 variable "start_cleanup" {
-  type = bool
+  type        = bool
+  description = "Whether or not start the cleanup process"
 }
