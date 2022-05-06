17f512d60ae33ade63d0eff1bbc7a6b486e6e78a
Eddie Rowe <74205376+eddie-rowe@users.noreply.github.com>
2022-05-06 11:06:47 -0500

    Revert "Add EKS+HCP resources for API GW"
    This reverts commit e6b629f765a9678241afaa5ed15ef75f666248cb.

--- api-gateway/cloud/terraform/hvn.tf
+++
@@ -1,7 +0,0 @@
-# The HVN created in HCP
-resource "hcp_hvn" "main" {
-  hvn_id         = var.hvn_id
-  cloud_provider = "aws"
-  region         = var.hvn_region
-  cidr_block     = var.hvn_cidr_block
-}
