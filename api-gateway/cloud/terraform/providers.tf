e6b629f765a9678241afaa5ed15ef75f666248cb
Eddie Rowe <74205376+eddie-rowe@users.noreply.github.com>
2022-05-06 10:58:55 -0500

    Add EKS+HCP resources for API GW

---
+++ api-gateway/cloud/terraform/providers.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.43"
+    }
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = ">= 0.18.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.4.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.3.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.11.3"
+    }
+    kustomization = {
+      source  = "kbst/kustomization"
+      version = "0.7.2"
+    }
+    consul = {
+      source  = "hashicorp/consul"
+      version = "~> 2.14"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.vpc_region
+  default_tags {
+    tags = {
+      Repository = "tf-cloud-repo"
+    }
+  }
+}
+
+provider "hcp" {
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "consul" {
+  address = hcp_consul_cluster.main.consul_public_endpoint_url
+  token   = hcp_consul_cluster_root_token.token.secret_id
+}
