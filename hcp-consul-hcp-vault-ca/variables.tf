cdd48de39637df479e898d956b6340ed1de1d964
Christian Weber <christian.weber@hashicorp.com>
2022-08-31 16:59:57 -0500

    Remove default variable for region, add validation for non-opt-in regions with link to docs when error occurs

--- hcp-consul-hcp-vault-ca/variables.tf
+++ hcp-consul-hcp-vault-ca/variables.tf
@@ -1,6 +1,27 @@
 variable "region" {
-  default     = "us-east-1"
-  description = "Region in which to run this terraform code for the AWS Provider"
+  description = "AWS Region to deploy this terraform code"
+  validation {
+    condition = contains([
+      "eu-north-1",
+      "ap-south-1",
+      "eu-west-3",
+      "eu-west-2",
+      "eu-west-1",
+      "ap-northeast-3",
+      "ap-northeast-2",
+      "ap-northeast-1",
+      "sa-east-1",
+      "ca-central-1",
+      "ap-southeast-1",
+      "ap-southeast-2",
+      "eu-central-1",
+      "us-east-1",
+      "us-east-2",
+      "us-west-1",
+      "us-west-2"
+    ], var.region)
+    error_message = "The name of the region you entered is invalid. Please try again. \n Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html ."
+  }
 }
 
 variable "node_group_configuration" {
